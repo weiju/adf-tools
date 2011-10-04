2959b0cdb0ac80ebc3a27f866c793bb2fc656a12
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-10-04 13:37:01 -0700

    dropped maven support, supporting sbt 0.11.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,24 @@
 import sbt._
+import Keys._
 
 object MyBuild extends Build {
 
+  override lazy val settings = super.settings ++ buildSettings
+
+  def buildSettings = Seq(
+    organization := "org.dmpp",
+    version := "1.0",
+    scalaVersion := "2.9.1",
+    javacOptions in Compile ++= Seq("-target", "6", "-source", "6")
+  )
+
   lazy val root = Project("root", file(".")) aggregate(gui)
-  lazy val gui = Project("gui", file("adf-gui")) dependsOn(core, infolib)
+  lazy val gui = Project("gui", file("adf-gui")) settings(testDependencies :_*) dependsOn(core, infolib)
   lazy val infolib = Project("infolib", file("infolib")) dependsOn(core)
   lazy val core = Project("core", file("adf-core"))
 
-/*
-  lazy val core = project("adf-core", "adf-core")
-  lazy val infolib  = project("infolib",  "infolib", core)
-  lazy val gui  = project("adf-gui",  "adf-gui", core, infolib)
-  lazy val infoviewer  = project("infoviewer",  "infoviewer", infolib)
-  */
+  def testDependencies = libraryDependencies ++= Seq(
+    "org.scalatest" %% "scalatest" % "1.6.1",
+    "junit" % "junit" % "4.9")
 }
 
