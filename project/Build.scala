2f3256c2247e9140a439530c38baebfcc988ab71
Wei-ju Wu <weiju.wu@gmail.com>
2015-03-16 14:34:31 -0700

    Updated to Scala 2.11.6

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object MyBuild extends Build {
   def buildSettings = Seq(
     organization := "org.dmpp",
     version := "1.0",
-    scalaVersion := "2.11.4",
+    scalaVersion := "2.11.6",
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
     javacOptions in Compile ++= Seq("-target", "6", "-source", "6")
   )
